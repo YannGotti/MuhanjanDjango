23203c10b7ae999b8213c876947bd74b5764bf28
MIRON <1111111111aleksey46@gmail.com>
2022-04-14 20:44:27 +0300

    fix

--- muhanjan_loto/loto/urls.py
+++ muhanjan_loto/loto/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('managelobby/<int:lobby_id>', views.manage_page, name='manage lobby'),
-    path('enterlobby/<int:lobby_id>', views.enter_lobby_page, name='enter lobby'),
+    path('managelobby/<int:lobby_id>/', views.manage_page, name='manage lobby'),
+    path('enterlobby/<int:lobby_id>/', views.enter_lobby_page, name='enter lobby'),
     path('card/<int:lobby_id>/', views.get_game_card, name='my card'),
     path('managelobby/<int:lobby_id>/get_stream_page/', views.get_stream_page, name='get_stream_page'),
     path('managelobby/<int:lobby_id>/stop_game/', views.stop_game, name='stop_game'),
